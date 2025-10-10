39ec053a26dedcdf5fc517fa51546db09697a25b
Aaron Schelfo <schelfoaaron918@gmail.com>
2025-10-09 22:34:36 -0500

    Delete JavaScript Course /Module 1: Introduction to JavaScript /Lesson 1.2  directory

--- JavaScript Course /Module 1: Introduction to JavaScript /Lesson 1.2 /Comments.js
+++
@@ -1,8 +0,0 @@
-console.log(
-  // JavaScript gives a clarifying code
-  );
-console.log(
-/*
-The first time to multi-line the projects, which helps explain the importance of comments in terms of collaboration
-*/
-  );
