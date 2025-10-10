8635bf35590d8f567f69ff10fbf971d8f15f5326
Aaron Schelfo <schelfoaaron918@gmail.com>
2025-10-09 23:13:20 -0500

    StringConcatenation.js

---
+++ JavaScript Course /Module 1: Introduction to JavaScript /Session 1.5 /StringConcatenation.js
@@ -0,0 +1,4 @@
+console.log('Hello' + ' ' + 'Aaron'); // Middle Space
+console.log('I love ' + 'JavaScript'); // Front Space (1st string)
+console.log('Space' + 'ship'); // No Space
+console.log('Right' + ' Behind'); // Back Space (2nd string)
