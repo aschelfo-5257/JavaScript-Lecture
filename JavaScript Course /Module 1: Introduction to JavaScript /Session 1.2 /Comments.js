bb523b7a85b1e87eff681c6e88902d3b3d831877
Aaron Schelfo <schelfoaaron918@gmail.com>
2025-10-09 22:19:05 -0500

    Comments.js

---
+++ JavaScript Course /Module 1: Introduction to JavaScript /Session 1.2 /Comments.js
@@ -0,0 +1,8 @@
+console.log(
+  // JavaScript gives a clarifying code
+  );
+console.log(
+/*
+The first time to multi-line the projects, which helps explain the importance of comments in terms of collaboration
+*/
+  );
